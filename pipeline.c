f9b5851f5df91d76103992fcf161f97f5ad37e74
Ting-Rui, Wang <qwe50120@gmail.com>
2018-03-20 13:09:53 +0000

    Format Style

--- pipeline.c
+++ pipeline.c
@@ -10,17 +10,17 @@
 
 long neon_iteration;
 
-float32_t mul(float *source,float *weight);
+float32_t mul(float *source, float *weight);
 void create_input(int size);
 
 static long diff_in_us(struct timespec t1, struct timespec t2)
 {
     struct timespec diff;
     if (t2.tv_nsec-t1.tv_nsec < 0) {
-        diff.tv_sec  = t2.tv_sec - t1.tv_sec - 1;
+        diff.tv_sec = t2.tv_sec - t1.tv_sec - 1;
         diff.tv_nsec = t2.tv_nsec - t1.tv_nsec + 1000000000;
     } else {
-        diff.tv_sec  = t2.tv_sec - t1.tv_sec;
+        diff.tv_sec = t2.tv_sec - t1.tv_sec;
         diff.tv_nsec = t2.tv_nsec - t1.tv_nsec;
     }
     return (diff.tv_sec * 1000000.0 + diff.tv_nsec / 1000.0);
@@ -30,27 +30,28 @@ static long diff_in_us(struct timespec t1, struct timespec t2)
 int main(int argc, char *argv[])
 {
 
-    struct timespec start,end;
-    float source[SIZE] ;
-    float weight[SIZE] ;
+    struct timespec start, end;
+    float source[SIZE];
+    float weight[SIZE];
     char source_char[10];
     char weight_char[10];
     FILE *fptr;
     create_input(SIZE);
-    fptr = fopen(FILE_NAME,"r");
+    fptr = fopen(FILE_NAME, "r");
     for(int i = 0; i<SIZE; i++) {
-        fscanf(fptr,"%s %s %f %f\n",source_char,weight_char,&source[i],&weight[i]);
+        fscanf(fptr,"%s %s %f %f\n",source_char, weight_char, &source[i], &weight[i]);
     }
-    clock_gettime(CLOCK_REALTIME,&start);
-    printf("output:  %f\n",mul(source,weight));
-    clock_gettime(CLOCK_REALTIME,&end);
-    printf("spend:  %ld us\n",diff_in_us(start,end));
+    clock_gettime(CLOCK_REALTIME, &start);
+    printf("output:  %f\n",mul(source, weight));
+    clock_gettime(CLOCK_REALTIME, &end);
+    printf("spend:  %ld us\n",diff_in_us(start, end));
+    fclose(fptr);
     return 0;
 }
 
 float32_t mul(float *source,float *weights)
 {
-    float32x4_t in1_128,in2_128,sum1,sum2,prod;
+    float32x4_t in1_128, in2_128, sum1, sum2, prod;
     float32_t result[4];
     float32_t output = 0.0;
     int i;
@@ -66,6 +67,7 @@ float32_t mul(float *source,float *weights)
         output += result[0];
 #endif
 #ifdef NON_FLUSH
+        // prod[i] = prod[i] + in1_128[i] * in2_128 for i in 0 to 3
         prod = vmlaq_f32(prod, in1_128, in2_128);
 #endif
     }
@@ -75,7 +77,6 @@ float32_t mul(float *source,float *weights)
     vst1q_f32((float32_t *)result, sum2);
     output = result[0];
 #endif
-
     return output;
 }
 
@@ -89,7 +90,7 @@ void create_input(int size)
     fptr = fopen(FILE_NAME,"w");
     srand(time(NULL));
     for(int i = 0; i<size; i++) {
-        fprintf(fptr,"source weight %f %f\n",(float32_t)(rand()%4)+1,(float32_t)(rand()%4)+1);
+        fprintf(fptr,"source weight %f %f\n",(float32_t)(rand()%4)+1, (float32_t)(rand()%4)+1);
     }
     fclose(fptr);
 }
